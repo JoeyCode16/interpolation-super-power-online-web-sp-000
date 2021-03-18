1de67864b281aee5872ba2d5f40e950cfa2c3060
reliable-genre-2916 <joey16burke@gmail.com>
2021-03-18 21:54:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
 def display_rainbow(colors)
-(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts colors.join("'R','O','Y','G','B','I','V'")
+puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
