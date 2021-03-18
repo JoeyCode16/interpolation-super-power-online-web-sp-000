03d4ddd175d7fe1fdada8f3aca7a7f7906c96de1
reliable-genre-2916 <joey16burke@gmail.com>
2021-03-18 22:01:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-puts "R:#(colors[0]): red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+puts "R:#{colors[0]}: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
