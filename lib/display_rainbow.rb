66aeccdb2d7c4f356ef5301899029d5f5cbaa898
reliable-genre-2916 <joey16burke@gmail.com>
2021-03-18 21:53:00 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts colors.join("'R','O','Y','G','B','I','V'")
+end
---
+++ spec/lib/display_rainbow.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+def display_rainbow (colors)
+  binding.pry
+  # puts display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+end
